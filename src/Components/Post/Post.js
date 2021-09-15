9664c6942bb172cf39f2fb9f24151b6a412280b5
Ahmed Ramy <64094908+aahmedramy1@users.noreply.github.com>
2021-09-15 14:47:35 +0200

    z

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Post.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { changeLike } from "../../Slices/rootSlice";
 import { useDispatch, useSelector } from "react-redux";
 
